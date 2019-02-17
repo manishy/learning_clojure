1c75bcca8e9df464f3123f8b3ba51138e827308e
Manish <manishyadav12354@gmail.com>
2019-02-17 18:37:04 +0530

    Added my-map and my-filter function

--- exercises.clj
+++ exercises.clj
@@ -8,14 +8,29 @@
 
 ; my-map  -------------------------
 
-; (defn my-map [f coll]
-;   (loop [f c coll final []]
-;     (if (empty? c) final
-;         (recur (f (rest c) )) (conj final (f (first c))) ))
-;   )
-
 (defn my-map [f coll]
-  (if (empty? coll)))
+  (loop [c coll final []]
+    (if (empty? c) final
+        (recur (rest c) (conj final (f (first c)))
+               )
+        )
+    )
+)
+
+(my-map #(* % 2) '(1 2 3 4))
+(my-map even? [1 2 3 4])
 
 
-(my-map f [2 3 4])
+; my-filter  -----------------------
+
+(defn my-filter [f coll]
+  (loop [c coll final []]
+    (if (empty? c) final
+        (
+         if (f (first c)) (recur (rest c) (conj final (first c))) (recur (rest c) final)
+          )
+        )
+    )
+  )
+
+(my-filter even? '(1 2 3 4 5))
