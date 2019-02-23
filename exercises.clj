e4203642e52b9786e997782c916bd314c18a3e09
Manish <manishyadav12354@gmail.com>
2019-02-23 14:11:42 +0530

    Added my-drop and my-reduce

--- exercises.clj
+++ exercises.clj
@@ -6,6 +6,16 @@
 
 (factorial 4)
 
+; my-drop -------------------------
+(defn my-drop [till coll]
+  (loop [count till c coll]
+    (if (or (empty? c) (= count 0))
+      c
+      (recur (- count 1) (rest c)))))
+
+(my-drop 2 [1 2 3 4 5])
+(drop 2 [])
+
 ; my-map  -------------------------
 
 (defn my-map [f coll]
@@ -21,16 +31,46 @@
 (my-map even? [1 2 3 4])
 
 
+
 ; my-filter  -----------------------
 
 (defn my-filter [f coll]
   (loop [c coll final []]
-    (if (empty? c) final
-        (
-         if (f (first c)) (recur (rest c) (conj final (first c))) (recur (rest c) final)
-          )
-        )
+    (if (empty? c)
+      final
+      (if (f (first c))
+        (recur (rest c) (conj final (first c)))
+        (recur (rest c) final)))))
+
+(my-filter even? '(1 2 3 4 5))
+
+; my-reduce   ------------------------
+(defn my-reduce [f coll]
+  (loop [c coll final 0]
+    (
+     if (empty? c) final (recur (rest c) (f final (first c)))
+     )
     )
   )
 
-(my-filter even? '(1 2 3 4 5))
+
+
+(my-reduce - '(1 2 3 4))
+
+
+
+
+
+
+; (def foo (comp (partial take-while not-nil?) (partial iterate remove-first-and-last)))
+; (foo "albla")
+
+
+; is-prime?  ----------------------------------------------
+; find factors of given number
+
+; (def is-prime? ())
+
+; (rem 3 2)
+; comp , partial , apply ,repeat,  
+
