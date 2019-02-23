3428dfcd1044b06c874171e3c65db551b2b22bd7
Manish <manishyadav12354@gmail.com>
2019-02-23 13:27:51 +0530

    Added is-prime? function

--- my-functions.clj
+++ my-functions.clj
@@ -8,9 +8,6 @@
 
 (my-square 6)
 
-
-
-
 ; is-palindrome?   ---------------------------------------
 
 (def remove-first-and-last (comp butlast rest))
@@ -38,3 +35,35 @@
 (is-palindrome? "1")
 (is-palindrome? [1 2 3 2 1])
 
+
+; is-prime?   ---------------------------------------
+
+(def is-equal-to-two? (partial = 2))
+
+(def possible-factors-of (comp rest (partial range) inc))
+
+(def is-divisible(comp zero? (partial apply rem)))
+
+(def repeat-last (comp (partial apply repeat) (juxt last last)))
+
+(def make-pair (partial apply map list)) 
+
+(def pair-of-divisor-and-dividend (comp make-pair (juxt repeat-last identity)))
+
+(def filter-actual-factors (comp (partial map second) (partial filter is-divisible) pair-of-divisor-and-dividend))
+
+(def is-prime? (comp 
+                is-equal-to-two?
+                count
+                filter-actual-factors
+                possible-factors-of))
+
+(is-prime? 2)
+(is-prime? 2)
+(is-prime? 4)
+(is-prime? 5)
+(is-prime? 7)
+(is-prime? 11)
+(is-prime? 13)
+(is-prime? 17)
+(is-prime? 19)
